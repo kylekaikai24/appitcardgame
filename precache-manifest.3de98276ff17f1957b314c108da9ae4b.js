ad8252340b3b9f53bb830fc96df7eed74a9bc286
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-05 10:55:53 +0000

    Deploy kylekaikai24/appitcardgame to github.com/kylekaikai24/appitcardgame.git:gh-pages

---
+++ precache-manifest.3de98276ff17f1957b314c108da9ae4b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2789cffb6ffe988941675701657a3e55",
+    "url": "/appitcardgame/index.html"
+  },
+  {
+    "revision": "1a03a66d7d05f261de4b",
+    "url": "/appitcardgame/static/css/main.77f60aa8.chunk.css"
+  },
+  {
+    "revision": "326f8ce48d44b6f9126a",
+    "url": "/appitcardgame/static/js/2.42966367.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/appitcardgame/static/js/2.42966367.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a03a66d7d05f261de4b",
+    "url": "/appitcardgame/static/js/main.b38b9f67.chunk.js"
+  },
+  {
+    "revision": "fc19a7dcd4c072584607",
+    "url": "/appitcardgame/static/js/runtime-main.583236ee.js"
+  },
+  {
+    "revision": "cf3b4bb75bf2b57fb8623ef1ae0995fc",
+    "url": "/appitcardgame/static/media/AlienLeague.cf3b4bb7.woff"
+  },
+  {
+    "revision": "d1c9d34d974b36826c7d312f7596c382",
+    "url": "/appitcardgame/static/media/AlienLeague.d1c9d34d.woff2"
+  },
+  {
+    "revision": "1669af11559a8d827e52ccfca05d3443",
+    "url": "/appitcardgame/static/media/cardback.1669af11.png"
+  },
+  {
+    "revision": "47c7c3e96201ca8ca3c823a813c171ea",
+    "url": "/appitcardgame/static/media/cardfront.47c7c3e9.png"
+  },
+  {
+    "revision": "4b3a2baac1dcb39bdb9cb9757a19ebe3",
+    "url": "/appitcardgame/static/media/planet.4b3a2baa.gif"
+  },
+  {
+    "revision": "2028b6518cf1876c24f0f182a52521f3",
+    "url": "/appitcardgame/static/media/spinner.2028b651.gif"
+  }
+]);
